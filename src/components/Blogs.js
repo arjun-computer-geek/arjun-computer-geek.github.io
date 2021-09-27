6e8d6b52c70ad8777e70bff771593b06ff5eab1b
arjun <arjun2000raj@gmail.com>
2021-09-27 21:19:24 +0530

    modify blogs page

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -14,6 +14,7 @@ const Blogs = () => {
                         <p>I wrote some blogs on  tech related Topics. It helps me document my journey of learning. Go and check it out. 👇👇👇👇 </p>
                     </div>
                     <div className="services-body">
+
                         <div className="services-item">
                             <Link style={{border: "none"}} to="/arch_guide">
                                 <div className="icon"><i className="fa fa-cogs fa-3x"></i></div>
@@ -23,33 +24,11 @@ const Blogs = () => {
                         </div>
 
                         <div className="services-item">
-                            <div className="icon"><i className="fa fa-cogs fa-3x"></i></div>
-                            <h2>Website Design & Re-design</h2>
-                            <p>I Design a new website as well as Re-design old website.I can modify your website as per your needs.</p>
-                        </div>
-
-                        <div className="services-item">
-                            <div className="icon"><i className="fa fa-cogs fa-3x"></i></div>
-                            <h2>Html5 Website Development</h2>
-                            <p>All HTML5-based web app solutions built by me are both secure and sustainable, constructed in such a way they can be easily maintained and scaled as needed.</p>
-                        </div>
-
-                        <div className="services-item">
-                            <div className="icon"><i className="fa fa-cogs fa-3x"></i></div>
-                            <h2>Responsive Design</h2>
-                            <p>Customer-focused, creating mobile-friendly responsively designed experiences that deliver tangible business results, I helps brands navigate any and all facets of the digital landscape.</p>
-                        </div>
-
-                        <div className="services-item">
-                            <div className="icon"><i className="fa fa-cogs fa-3x"></i></div>
-                            <h2>Contentfull Development</h2>
-                            <p>Client-focused, customer-centric, creating digital solutions that deliver tangible business results, I help brands achieve more in the ever-changing digital landscape.</p>
-                        </div>
-
-                        <div className="services-item">
-                            <div className="icon"><i className="fa fa-cogs fa-3x"></i></div>
-                            <h2>eCommerce Development</h2>
-                            <p>eCommerce development services that are customer-focused, creating e-shopping experiences deliver tangible business results, I help brands navigate any and all facets of the digital commerce landscape.</p>
+                            <Link style={{border: "none"}} to="/arch_guide">
+                                <div className="icon"><i className="fa fa-cogs fa-3x"></i></div>
+                                <h2>Arch Installation Guide</h2>
+                                <p>A Simple and Step By Step Installation Guide to install Arch Linux along with the Windows 10 [Dual boot].</p>
+                            </Link>
                         </div>
 
                     </div>
