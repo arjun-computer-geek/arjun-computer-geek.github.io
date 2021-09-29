16d7814456ffd3281b728b77ae24fd8956c45e63
arjun <arjun2000raj@gmail.com>
2021-09-30 01:34:25 +0530

    fix- Fixing routing issue

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-import { Switch } from 'react-router'
 import Header from './components/layout/Header'
 import Footer from './components/layout/Footer'
 import Home from './components/home/Home'
@@ -12,15 +11,13 @@ import ImportantLinuxCommand from './components/blogs/importLinuxCommand/Importa
 const App = () => {
   return (
     <Router>
-      <Header />
-      <Switch>
-        <Route path="/gh-pages-url" component={Home} exact />
-        <Route path="/gh-pages-url/projects" component={Projects} exact />
-        <Route path="/gh-pages-url/blogs" component={Blogs} exact />
-        <Route path="/gh-pages-url/arch_guide" component={ArchGuide} exact />
-        <Route path="/gh-pages-url/important-linux-command" component={ImportantLinuxCommand} exact />
-      </Switch>
-      <Footer />
+      <Header/>
+      <Route path = "/" component={Home} exact />
+      <Route path = "/projects" component={Projects} exact />
+      <Route path = "/blogs" component={Blogs} exact />
+      <Route path = "/arch_guide" component={ArchGuide} exact />
+      <Route path = "/important-linux-command" component={ImportantLinuxCommand} exact />
+      <Footer/>
     </Router>
   )
 }
